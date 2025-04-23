70012ed970c357412593005ca85b210e98e342f7
smile <1813244681@qq.com>
2025-04-23 15:58:44 +0800

    增加服务

--- server/__init__.py
+++ server/__init__.py
@@ -1,1 +1,11 @@
-print("Hello World")
+import os
+from flask import Flask, request, jsonify
+
+from flask_cors import CORS
+
+
+app = Flask(__name__)
+CORS(app)
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
