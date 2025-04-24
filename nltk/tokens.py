e1744c6ca96eed6366f9d2ce525e915642f21c40
smile <1813244681@qq.com>
2025-04-24 13:52:18 +0800

    新增文件

---
+++ nltk/tokens.py
@@ -0,0 +1,11 @@
+import nltk
+nltk.download('punkt_tab')
+from nltk.tokenize import word_tokenize
+
+
+
+
+sample_text = 'I love programming!'
+tokens = word_tokenize(sample_text)
+
+print('Tokens:', tokens)
